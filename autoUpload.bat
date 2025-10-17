658088d317b4f5b40523786aaa8c02b87cf12313
Zihan Zhang <zhangzihan.cs@gmail.com>
2025-10-17 13:08:10 +0800

    upload a blong

--- autoUpload.bat
+++ autoUpload.bat
@@ -1,18 +1,15 @@
 @echo off
 REM 自动提交并推送更改脚本
 
-REM 设置提交信息
-set /p commitMessage=请输入提交信息: 
-
 REM 添加所有更改
 git add .
 
 REM 提交更改
-git commit -m "%commitMessage%"
+git commit -m "upload a blong"
 
 REM 推送到远程仓库
 git push origin master
 
 REM 提示推送完成
-echo 推送完成！
+echo push success!
 pause
